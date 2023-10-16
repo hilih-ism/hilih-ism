c885809d8e35546e925817fc283d1226c1ab6c23
Hildana Shewaneka <112654424+hilih-ism@users.noreply.github.com>
2023-10-16 17:55:15 +0300

    added card

--- hilih-blogs/src/App.jsx
+++ hilih-blogs/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar.jsx'
+import Card from "./components/card.jsx";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar/>
+        <Card/>
         <button onClick={() => setCount((count) => count + 1)}>
           buy me a potato {count}
         </button>
---
+++ hilih-blogs/src/components/card.jsx
@@ -0,0 +1,9 @@
+function Card(){
+    return(
+        <div className="card-1">
+            <h4>Title</h4>
+            <h6>Lorem ipsum Lorem ipsum Lorem <br/>ipsum Lorem ipsum Lorem ipsum</h6>
+        </div>
+    )
+}
+export default Card
