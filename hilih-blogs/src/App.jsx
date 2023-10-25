129304224ec5f307543a978ae96acb4727630981
Hildana Shewaneka <112654424+hilih-ism@users.noreply.github.com>
2023-10-25 09:50:27 +0300

    added cards with hover + tailwind

--- hilih-blogs/src/App.jsx
+++ hilih-blogs/src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar.jsx'
 import Card from "./components/card.jsx";
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
   return (
     <>
       <Navbar/>
-        <Card/>
-        <button onClick={() => setCount((count) => count + 1)}>
-          buy me a potato {count}
-        </button>
+        <div className={"card-container"}>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
      </>
   )
 }
--- hilih-blogs/src/components/card.jsx
+++ hilih-blogs/src/components/card.jsx
@@ -3,6 +3,11 @@ function Card(){
         <div className="card-1">
             <h4>Title</h4>
             <h6>Lorem ipsum Lorem ipsum Lorem <br/>ipsum Lorem ipsum Lorem ipsum</h6>
+            <div className={"info"}>
+                <img src={"/vendor.png"} alt={"user"} width={40}/>
+                <h7>HiliH</h7><br/>
+                <h7>29/12/2023</h7>
+            </div>
         </div>
     )
 }
